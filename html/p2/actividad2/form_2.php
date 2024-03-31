fbc0364246d6135934f97d078ea2c92a00328b73
JuanDiegoReyes <130397141+JuanDiegoReyes@users.noreply.github.com>
2024-03-31 17:50:29 -0600

    Add files via upload

---
+++ html/p2/actividad2/form_2.php
@@ -0,0 +1,9 @@
+<?php
+echo "<b>Nombre:</b> {$_POST['nombre']}{$_POST['ap']}{$_POST['am']}<br>";
+echo "<b>Usuario:</b> {$_POST['usuario']}<br>";
+echo "<b>Contraseña:</b> {$_POST['contraseña']}<br>";
+echo "<b>Fecha de nacimiento:</b> {$_POST['fechan']}<br>";
+echo "<b>Domicilio:</b> {$_POST['colonia']}, {$_POST['cs']}, {$_POST['ciudad']}, {$_POST['pais']} <br>";
+echo "<b>R.F.C.:</b> {$_POST['rfc']}<br>";
+echo "<b>C.P:</b> {$_POST['cp']}<br>";
+?>
